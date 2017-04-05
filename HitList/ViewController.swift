38c01acd5e1665f95ff6bda5b223ef217ab2a538
Vivek <vivek.shah@zymr.com>
2017-04-05 12:05:29 +0530

    Test commit

---
+++ HitList/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by Vivek Shah on 4/5/17.
+//  Copyright © 2017 Vivek Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tblViewMain: UITableView!
+    @IBAction func didTapAddData(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
